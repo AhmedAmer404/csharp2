b0ed6a41ea03e0c73f34a1c26a305c95405f449e
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-01 16:39:55 +0200

    question1

--- c#basics3/Program.cs
+++ c#basics3/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Assignment
+{
+    public class  Program 
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            try
+            {
+                int zero = 0;
+                int result = 5 / zero;
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("cant divide by zero");
+
+            }
+            finally
+            {
+                Console.WriteLine("done");
+            }
+            #endregion
+        }
+    }
+}
